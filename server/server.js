5cb3fbbdbb388ca5f58406b56fee6fdd6c41e0c3
K M <kevin m>
2019-05-31 01:28:14 -0500

    moved images server side

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const app = express();
 
-
+// handles cors
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
+// serve static files
+app.use(express.static('public'));
+
 app.get('/address', (req, res) => {
-    res.json({ data: 'hi' });
+    res.json({
+        header: 'Hilton Chicago',
+        line1: '720 South Michigan Avenue',
+        line2: 'Chicago, Illinois, 60605',
+        phoneNum: '1-132-922-4400'
+    });
 });
 
 app.set('port', 4200);
